2c89d74974b6b50ea669b04de057e8d0865d4c6e
Jephthah Orobia <sirjhep@gmail.com>
2015-04-24 09:58:59 +0800

    Temporary Fix, adding a space and "a" after each new MX

--- MathX/plugin.js
+++ MathX/plugin.js
@@ -11,17 +11,20 @@ tinymce.PluginManager.add('MathX', function (editor, url) {
 
 	// an object that will not return anything, MathX object, treated as global, but cannot be access outside
 	var MX = {
+    
 		//when this is true, it means that MX is on edit mode.
 		isActive : false,
-
+    
 		// insert a new equation
 		insert : function () {
 			if (MX.isActive)
 				return;
+      var AllJax = editor.getWin().MathJax.Hub.getAllJax();
+      this.jaxCount = 3 + AllJax.length;
 			var content = editor.selection.getContent();
 			editor.insertContent('<span class="MathJax">');
 			var bm = editor.selection.getBookmark();
-			editor.insertContent(content + '</span> ');
+			editor.insertContent(content + '</span>');
 			editor.selection.moveToBookmark(bm);
 		},
 
@@ -33,12 +36,16 @@ tinymce.PluginManager.add('MathX', function (editor, url) {
 					this.frame.remove();
 					this.table.remove();
 				} else {
-					var txtnode = W.document.createTextNode('`' + $(this.MXin).text() + '`');
+          var source = $(this.MXin).text();
+					var txtnode = W.document.createTextNode('`' + source + '`');
 					this.frame.parentNode.replaceChild(txtnode, this.table);
 					this.frame.remove();
 					W.MathJax.Hub.Queue(function () {
 						W.MathJax.Hub.Typeset();
 					});
+          var AllJax = W.MathJax.Hub.getAllJax();
+          for(var j in AllJax)
+            if(AllJax[j].originalText == source) this.jax = AllJax[j];
 				}
 			} else {
 				if ($(this.MXin).text() === '') {
@@ -75,9 +82,15 @@ tinymce.PluginManager.add('MathX', function (editor, url) {
 		//actions after closing or ending MX.
 		postHook : function () {
 			this.isActive = false;
-			var frame = this.frame;
-			if (this.jax)
-				editor.selection.setCursorLocation(frame.nextSibling.nextSibling, 1);
+      var D = editor.getDoc();
+      var script = D.getElementById(this.jax.inputID);
+      var txt = D.createTextNode(' a');
+      if(script.nextSibling)
+        script.parentNode.insertBefore(txt, script.nextSibling)
+      else
+        script.parentNode.appendChild(txt);
+      editor.selection.select(txt);
+      editor.selection.collapse();
 		},
 
 		init : function (frame) {
@@ -120,7 +133,6 @@ tinymce.PluginManager.add('MathX', function (editor, url) {
 	})();
 
 	//load MathJax script into editor's iframe
-	//load a style which is intended for the MathX.editor.input
 	editor.on('init', function (ed) {
 		var script = this.getDoc().createElement("script");
 		script.type = "text/javascript";
@@ -176,7 +188,19 @@ tinymce.PluginManager.add('MathX', function (editor, url) {
 		else if (e.keyCode == 61 && e.altKey)
 			MX.insert();
 	});
-
+  
+/*   editor.on('submit', function(e){
+    var W = editor.getWin(), D = editor.getDoc();
+    $(".MathJax_Preview").remove();
+    $(".MathJax").remove();
+    var AllJax = W.MathJax.Hub.getAllJax();
+    for(var j in AllJax){
+      var script = D.getElementById(AllJax[j].inputID);
+      script.parentNode.replaceChild(D.createTextNode(AllJax[j].originalText), script);
+    }
+    $("body>div").remove();
+  }); */
+  
 	// Add a button that opens a window
 	editor.addButton('MathX', {
 		text : 'f(x)',
